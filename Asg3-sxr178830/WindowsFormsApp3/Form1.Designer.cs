974329c8b30906c970625c960d0ef80ef9076c64
Swetha Ramaswamy <swetharamaswamy66@gmail.com>
2018-02-12 22:34:15 -0600

    Add files via upload

---
+++ Asg3-sxr178830/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.outputbox = new System.Windows.Forms.RichTextBox();
+            this.run = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 688);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1596, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 17);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(644, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BROWSE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filename
+            // 
+            this.filename.Location = new System.Drawing.Point(240, 123);
+            this.filename.MaxLength = 1000;
+            this.filename.Name = "filename";
+            this.filename.Size = new System.Drawing.Size(385, 26);
+            this.filename.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(422, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WELCOME TO THE EVALUATOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listView1
+            // 
+            this.listView1.BackgroundImageTiled = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader2,
+            this.columnHeader1});
+            this.listView1.Location = new System.Drawing.Point(0, 341);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1187, 323);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "NAME";
+            this.columnHeader5.Width = 120;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "PHONENO";
+            this.columnHeader6.Width = 95;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "ADDRESS";
+            this.columnHeader7.Width = 150;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "CITY,STATE,ZIPCODE";
+            this.columnHeader8.Width = 193;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "EMAIL";
+            this.columnHeader2.Width = 106;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "PROOF";
+            this.columnHeader1.Width = 82;
+            // 
+            // outputbox
+            // 
+            this.outputbox.Location = new System.Drawing.Point(1206, 55);
+            this.outputbox.Name = "outputbox";
+            this.outputbox.Size = new System.Drawing.Size(365, 609);
+            this.outputbox.TabIndex = 6;
+            this.outputbox.Text = "";
+            // 
+            // run
+            // 
+            this.run.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.run.Location = new System.Drawing.Point(453, 194);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(124, 52);
+            this.run.TabIndex = 7;
+            this.run.Text = "RUN";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1206, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "OUTPUT:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1596, 710);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.outputbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filename);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Evaluator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.RichTextBox outputbox;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Asg3-sxr178830/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        //many variables declared globally for the purpose of calculations
+        public int[] backspace= { 0,0,0,0,0,0,0,0,0,0};
+        public string[] entrytimes= {"0", "0" , "0" , "0" , "0" , "0" , "0" , "0", "0", "0", "0", "0" };
+        public string[] savetimes = { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"  };
+        public double[] times = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0 };
+        public double[] inter = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double totaltime;
+        double averagetime;
+        double averagespan;
+        int val = 0;
+
+        public int i = 0;
+        public List<String> information = new List<String>();
+
+        public object SafeConvert { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bool isDataLoaded = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //InitializeComponent();
+            //listView1.Columns[1].Text = "Column 1";
+
+        }
+
+    // the following is when the browse button is clicked
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {//try catch basically helps to avoid the system to crash when an invalid file type input has been selected
+                DialogResult result = openFileDialog1.ShowDialog();// used for having the browse buttons
+                string abcd;
+                bool isDataLoaded = true;
+                status.Text = "Choosing a file";// outputting results on the status bar
+                if (result == DialogResult.OK) // Test result.
+                {
+                    status.Text = "File Selected";// comments in the status bar allwoing the user to know what is going on in the background
+
+
+                    filename.Text = openFileDialog1.FileName;
+                    String file = openFileDialog1.FileName;
+
+                    try
+                    {
+                        string[] readText = File.ReadAllLines(file);// opening the file that has been selected
+
+                        status.Text = "Updated list view";//comments in the status bar
+                        foreach (string s in readText)
+
+                        {
+                            if (s != "\r\n")
+                            {
+                                char[] delimiterChars = { '\t' };// reading the file when selected and giving the output on the list view
+                                string name = "", phone = "";
+                                string address = "", csz = "", proof = "";
+                                string mailid = "";
+                                string[] words = s.Split(delimiterChars);
+
+                                name = words[0] + " " + words[1] + " " + words[2];
+                                phone = words[3];
+                                address = words[4] + " " + words[5];
+                                csz = words[6] + " " + words[7] + " " + words[8];
+                                mailid = words[9];
+                                proof = words[10];
+
+                                string[] row = { name, phone, address, csz, mailid, proof };
+                                var listViewItem = new ListViewItem(row);// entering the values in the listview frm the file selected
+                                listView1.Items.Add(listViewItem);
+                                entrytimes[i] = words[12];
+                                savetimes[i] = words[13];
+
+                                if (int.TryParse(words[14], out backspace[i]))
+                                {
+
+
+                                }
+
+
+
+
+                                i = i + 1;
+
+
+                            }
+                            else
+                            {
+                                continue;
+                            }
+
+                        }
+
+
+                        for (int k = 0; k < 10; k++)
+                        {
+                            int random = 0;
+                            random = backspace[k];
+                            val += random;
+                        }
+                        status.Text = "Opened a file for evaluation";// giving comments on the status bar for the user to know
+                    }
+                    catch (IOException)
+                    {
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                status.Text = "Invalid File Type";// comment when an invalid data type has been selected
+            }
+
+}
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        //this is the class which represents all the actions that take place when the run button is clicked
+        private void run_Click(object sender, EventArgs e)
+        {
+            status.Text = "Running the Evaluator";
+            timeconversion();
+            double min = 1000.0;
+            double intermax =0.0;
+            double max = 0.0;
+            double intermin =1000.0;
+            
+            for(int l=0;l<backspace.Length;l++)
+            {
+                if(times[l]>max)
+                {
+                    max = times[l];
+                }
+                if(times[l]<min)
+                {
+                    min = times[l];
+                }
+
+            }
+            for (int w = 0; w < backspace.Length; w++)
+            {
+                if (inter[w] > intermax)
+                {
+                    intermax = inter[w];
+                    
+                }
+                if (inter[w] < intermin)
+                {
+                    intermin = inter[w];
+                }
+
+            }
+
+            TimeSpan intmax = TimeSpan.FromSeconds(intermax);
+            TimeSpan intmin = TimeSpan.FromSeconds(intermin);
+
+            TimeSpan t = TimeSpan.FromSeconds(max);
+            TimeSpan span = TimeSpan.FromSeconds(min);
+            TimeSpan mn = TimeSpan.FromSeconds(totaltime);
+            TimeSpan avgr = TimeSpan.FromSeconds(averagetime);
+            TimeSpan intavg = TimeSpan.FromSeconds(averagespan);
+            System.IO.StreamWriter file = new System.IO.StreamWriter(@"CS6326Asg3.txt", true);
+
+            outputbox.AppendText("NUMBER OF RECORDS:= " + backspace.Length + "\r\n");// printing the total number of records in the file
+            file.Write("NUMBER OF RECORDS:= " + backspace.Length + "\r\n");
+            outputbox.AppendText("\r\n");
+            outputbox.AppendText("MAXIMUM ENTRY TIME:="+ string.Format("{0}:{1}", t.Minutes, t.Seconds) + "\r\n");// print the maximum time taken for entry
+            file.Write("MAXIMUM ENTRY TIME:=" + string.Format("{0}:{1}", t.Minutes, t.Seconds) + "\r\n");
+            outputbox.AppendText("\r\n");
+            outputbox.AppendText("MINIMUM ENTRY TIME:=" + string.Format("{0}:{1}", span.Minutes, span.Seconds) + "\r\n");// will print the minium time taken for entry
+            file.Write("MINIMUM ENTRY TIME:=" + string.Format("{0}:{1}", span.Minutes, span.Seconds) + "\r\n");
+            outputbox.AppendText("\r\n");
+            outputbox.AppendText("AVERAGE ENTRY TIME:=" + string.Format("{0}:{1}", avgr.Minutes, avgr.Seconds) + "\r\n");// will print the value of the average time taken for entry
+            file.Write("AVERAGE ENTRY TIME:=" + string.Format("{0}:{1}", avgr.Minutes, avgr.Seconds) + "\r\n");
+            outputbox.AppendText("\r\n");
+            outputbox.AppendText("TOTAL TIME:=" +string.Format("{0}:{1}",mn.Minutes,mn.Seconds) + "\r\n");// this will print the total time taken for entry
+            file.Write("TOTAL TIME:=" + string.Format("{0}:{1}", mn.Minutes, mn.Seconds) + "\r\n");
+            outputbox.AppendText("\r\n");
+            outputbox.AppendText("MAXIMUM INTER-RECORD TIME:=" + string.Format("{0}:{1}", intmax.Minutes, intmax.Seconds) + "\r\n");// this will print the maximum inter record time
+            file.Write("MAXIMUM INTER-RECORD TIME:=" + string.Format("{0}:{1}", intmax.Minutes, intmax.Seconds) + "\r\n");
+            outputbox.AppendText("\r\n");
+            outputbox.AppendText("MINIMUM INTER-RECORDS TIME:=" + string.Format("{0}:{1}", intmin.Minutes, intmin.Seconds) + "\r\n");// this will print the minimum inter record time
+            file.Write("MINIMUM INTER-RECORDS TIME:=" + string.Format("{0}:{1}", intmin.Minutes, intmin.Seconds) + "\r\n");
+            outputbox.AppendText("\r\n");
+            outputbox.AppendText("AVERAGE INTER-RECORD TIME:=" + string.Format("{0}:{1}", intavg.Minutes, intavg.Seconds) + "\r\n");// this will print the average inter record time
+            file.Write("AVERAGE INTER-RECORD TIME:=" + string.Format("{0}:{1}", intavg.Minutes, intavg.Seconds) + "\r\n");
+            outputbox.AppendText("\r\n");
+            outputbox.AppendText("NUMBER OF BACKSPACES:= " + val + "\r\n");// this will print the number of backspaces
+            file.Write("NUMBER OF BACKSPACES:= " + val + "\r\n");
+            file.Close();
+        }
+        public void timeconversion()
+        {//this method has all the time related functions 
+            double avg=0.0,avv=0.0;
+            for (int m = 0; m < backspace.Length; m++)
+            {
+                //string start = entrytimes[m];
+                //DateTime startTime = entrytimes[m];
+
+                //DateTime endTime = saveTime[m];
+
+                //TimeSpan span = endTime.Subtract(startTime);
+                TimeSpan duration = DateTime.Parse(savetimes[m].ToString()).Subtract(DateTime.Parse(entrytimes[m].ToString()));// calculation of the times for entries
+                times[m] = duration.TotalSeconds;
+                if (m <9)
+                {
+                    TimeSpan interspan = DateTime.Parse(entrytimes[m+1].ToString()).Subtract(DateTime.Parse(savetimes[m].ToString()));// calculating the times between two records
+                    inter[m] = interspan.TotalSeconds;
+                   
+                    
+                    
+                
+                }
+                else 
+                {
+                    continue;
+
+                }
+                
+            
+            }
+            for(int m = 0; m < backspace.Length; m++)
+            {
+                avg += times[m];
+
+            }
+            averagetime = avg / backspace.Length;
+            for (int m = 0; m < backspace.Length; m++)
+            {
+                avv += inter[m];
+
+            }
+            averagespan = avv / backspace.Length;
+            TimeSpan dur = DateTime.Parse(savetimes[9].ToString()).Subtract(DateTime.Parse(entrytimes[0].ToString()));// calculating the time taken for all the records to enter
+            totaltime = dur.TotalSeconds;
+
+
+        }
+    }
+}
